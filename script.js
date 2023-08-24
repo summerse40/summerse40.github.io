3081439cd4d9f4a5fba132286d9c0ef7e446bb6f
Emily Summers <141523772+summerse40@users.noreply.github.com>
2023-08-24 12:22:21 -0500

    Adjustments
    Hamburger Menu

--- script.js
+++ script.js
@@ -137,3 +137,9 @@ function myFunction1() {
         x.className = x.className.replace(" w3-show", "");
     }
 }
+const mobileMenuButton = document.getElementById("mobile-menu");
+const navList = document.querySelector(".nav-list");
+
+mobileMenuButton.addEventListener("click", () => {
+    navList.classList.toggle("active");
+});
